bea2cc163a05d9ee1bbfc6f37f071a463e929406
bigode <ian.tonete@escola.pr.gov.br>
2025-10-24 14:47:56 +0000

    lampada.final

---
+++ script.js
@@ -0,0 +1,13 @@
+const lampada = document.getElementById("lampada");
+
+lampada.addEventListener("mouseover",acender);
+lampada.addEventListener("mouseover",apagar);
+
+function acender(){
+    lampada.src = "on.png";
+    document.body.style.background = "#ffeb3b22"
+}
+function apagar(){
+    lampada.src = "off.png";
+    document.body.style.background = "#222"
+}
